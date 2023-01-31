94b4701261c8bc1f09d7140356aaac11f724bb86
eCoolGe <eCoolGe@mail.ru>
2023-01-31 15:30:32 +0300

    add cart functions and revival counter

--- js/pageContentRender.js
+++ js/pageContentRender.js
@@ -1,6 +1,8 @@
+let cart = [];
+
 const pageContentRender = (target) => {
-    // console.log(event.target)
-    let pageName = target.id
+
+    let pageName = target.id // or event.target
     let childrensParentNav = target.parentElement.children
 
     for(let i = 0; i < childrensParentNav.length; i++) {
@@ -30,7 +32,7 @@ const pageContentRender = (target) => {
                     document.querySelector(".product-list").innerHTML +=
                         `
                         <article class="item">
-                            <img class="item__shop-logo" src="${marketImage}" alt="">
+                        ${marketImage ? `<img class="item__shop-logo" src="${marketImage}" alt="">` : ``}                   
                             <div class="item__outer-circle">
                                 <div class="item__inner-circle">
                                     <img src="${data.menu[i].image}" alt="">
@@ -43,16 +45,28 @@ const pageContentRender = (target) => {
                                 <p>${data.menu[i].description}</p>
                             </div>
                             <p class="item__price">Цена: ${data.menu[i].price} ₽</p>
-                            <div class="item__counter-block">
+                            <div class="item__counter-block noselect">
                                 <p>КОЛИЧЕСТВО</p>
                                 <div class="counter-block__counter">
-                                    <span class="counter__minus">—</span>
+                                    <span onclick="counterDecrement(this)" class="counter__minus">—</span>
                                     <label>
-                                        <input type="text" value="1"/>
+                                        <input oninput="counterChange(this)" type="text" value="1"/>
                                     </label>
-                                    <span class="counter__plus">+</span>
+                                    <span onclick="counterIncrement(this)" class="counter__plus">+</span>
                                 </div>
-                                <button>В КОРЗИНУ</button>
+                                
+                                <button 
+                                ${cart.includes(data.menu[i].name) 
+                                    ? `class="active"` 
+                                    : ``
+                                }
+                                onclick="cartAdd(this)"
+                                >
+                                ${cart.includes(data.menu[i].name) 
+                                    ? `УБРАТЬ` 
+                                    : `В КОРЗИНУ`
+                                }
+                                </button>
                             </div>
                         </article>
                     `;
@@ -60,3 +74,42 @@ const pageContentRender = (target) => {
             }
         })
 }
+
+const cartAdd = (target) => {
+    let itemName = target.parentElement.parentElement.getElementsByClassName('item__name')[0].children[0].textContent
+
+    if (!cart.includes(itemName)) {
+        cart.push(itemName)
+        target.classList.add('active')
+        target.innerHTML = "УБРАТЬ"
+    } else {
+        const index = cart.indexOf(itemName);
+        if (index > -1) {
+            cart.splice(index, 1);
+        }
+        target.classList.remove('active')
+        target.innerHTML = "В КОРЗИНУ"
+    }
+}
+
+const counterIncrement = (target) => {
+    let counter = target.parentElement.getElementsByTagName('input')[0]
+    counter.innerHTML =  counter.value++
+}
+
+const counterDecrement = (target) => {
+    let counter = target.parentElement.getElementsByTagName('input')[0]
+    if (counter.value > 1) {
+        counter.innerHTML = counter.value--
+    }
+}
+
+const counterChange = (target) => {
+    let counter = target.parentElement.getElementsByTagName('input')[0]
+    counter.value = counter.value.replace(/[^\d.]/g, '');
+
+    if (counter.value < 1) {
+        counter.value = '1'
+    }
+}
+
